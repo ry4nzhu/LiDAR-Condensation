e3a4dc9a0b6b2300ca5f6223ca953c69e2ef1d2c
ry4nzhu <ryanzhu@umich.com>
2023-12-12 22:48:51 -0500

    update non-obj sample

--- tools/downsample_map.py
+++ tools/downsample_map.py
@@ -24,7 +24,7 @@ def random_downsample_loss(data_root, loss_map, td_logs, training_ids):
         print(pcd.shape, pcd_new.shape)
         original_size.append(pcd.shape[0])
         downsampled_size.append(pcd_new.shape[0])
-        # save_to_bin(train_file, pcd_new)
+        save_to_bin(train_file, pcd_new)
 
     print("downsampeld ratio:", np.sum(downsampled_size)/np.sum(original_size))
 
@@ -49,6 +49,48 @@ def random_downsample_loss_rank(data_root, loss_map, td_logs, training_ids):
     print("downsampeld ratio:", np.sum(downsampled_size)/np.sum(original_size))
 
 
+def object_downsample_loss_rank(data_root, loss_map, td_logs, training_ids):
+    lidar_path = os.path.join(data_root, 'training/velodyne')
+    label_path = os.path.join(data_root, 'training/label_2')
+    calib_path = os.path.join(data_root, 'training/calib')
+    original_size, downsampled_size = [], []
+    for idx, id in enumerate(training_ids):
+        train_file = os.path.join(lidar_path, "%06d.bin"%id)
+        label_file = os.path.join(label_path, '%06d.txt'%id)
+        calib_file = os.path.join(calib_path, '%06d.txt'%id)
+        calib = load_kitti_calib(calib_file)
+        boxes_velo, objs_type = read_objs2velo(label_file, calib['Tr_velo2cam'])
+        pcd = np.fromfile(train_file, dtype=np.float32).reshape(-1, 4)
+        print(idx, lidar_path, pcd.shape)
+        obj_points, non_obj_points = seperate_obj_points(pcd, boxes_velo, objs_type)
+
+        self_loss = td_logs[id][0]
+        downsample_ratio = loss_map.get_downsample_percentage_ranking(idx)
+        print(self_loss, downsample_ratio)
+
+        ## remove non-pedestrian, cyclist points
+        downsampled_shape = int(pcd.shape[0] * (1 - downsample_ratio))
+
+        if obj_points is not None:
+            remaining_target_point_num = downsampled_shape - obj_points.shape[0]
+        else:
+            remaining_target_point_num = downsampled_shape
+        if remaining_target_point_num > 0:
+            random_indices = np.random.randint(non_obj_points.shape[0], size=remaining_target_point_num)
+            points_remain = non_obj_points[random_indices]
+        else:
+            random_indices = np.random.randint(obj_points.shape[0], size=downsampled_shape)
+            obj_points = obj_points[random_indices]
+        if points_remain is not None:
+            obj_points = np.vstack([obj_points, points_remain])
+        # random_indices = np.random.randint(pcd.shape[0], size=downsampled_shape)
+        pcd_new = np.vstack(obj_points)
+        print(pcd.shape, pcd_new.shape)
+        original_size.append(pcd.shape[0])
+        downsampled_size.append(pcd_new.shape[0])
+        save_to_bin(train_file, pcd_new)
+
+    print("downsampeld ratio:", np.sum(downsampled_size)/np.sum(original_size))
 
 
 if __name__ == "__main__":
@@ -68,8 +110,12 @@ if __name__ == "__main__":
     #     linearLossMap, td_loss, training_sample_ids
     # )
 
+    # random_downsample_loss_rank(
+    #     "/y/datasets/kitti-downsample/KITTI-0.8-loss-rand-rank/",
+    #     linearLossMapRanking, td_loss, training_sample_ids
+    # )
 
-    random_downsample_loss_rank(
-        "/y/datasets/kitti-downsample/KITTI-0.8-loss-rand-rank/",
+    object_downsample_loss_rank(
+        "/y/datasets/kitti-downsample/KITTI-0.8-loss-obj-rank/",
         linearLossMapRanking, td_loss, training_sample_ids
     )
--- tools/util.py
+++ tools/util.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import open3d as o3d
 
 class_list = ['Car', 'Van' , 'Truck' , 'Pedestrian' , 'Person_sitting' , 'Cyclist' , 'Tram', 'Misc']
 
@@ -149,3 +150,161 @@ def read_objs2velo(label_file, Tr_velo2cam):
     objs_velo = np.array(objs_velo) #(n, 8)
 
     return objs_velo, objs_type 
+
+
+def rotz(t):
+    ''' Rotation about the z-axis. '''
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[c, -s,  0],
+                     [s,  c,  0],
+                     [0,  0,  1]])
+
+
+def get_box_arrow(dim):
+    h = dim[0]
+    # w = dim[1]
+    l = dim[2]
+    x = dim[3]
+    y = dim[4]
+    z = dim[5]
+    yaw = dim[6]
+
+    # get direction arrow
+    dx = l/2.0*np.cos(yaw)
+    dy = l/2.0*np.sin(yaw)
+    # a_start = [x, y, z+h]
+    # a_end = [x+dx, y+dy, z+h]
+    # arrow = [a_start, a_end]
+    arrow = [x, y, z+h, x+dx, y+dy, z+h] # [x0, y0, z0, x1, y1, z1], point0--->point1
+    return arrow
+
+
+def box_dim2corners(dim):
+    '''
+    dim: [h, w, l, x, y, z, yaw]
+
+    8 corners: np.array = n*8*3(x, y, z)
+    #         7 -------- 6
+    #        /|         /|
+    #       4 -------- 5 .
+    #       | |        | |
+    #       . 3 -------- 2            
+    #       |/         |/
+    #       0 -------- 1
+
+                ^ x(l)
+                |
+                |
+                |
+    y(w)        |
+    <-----------O
+    '''
+    h = dim[0]
+    w = dim[1]
+    l = dim[2]
+    x = dim[3]
+    y = dim[4]
+    z = dim[5]
+    yaw = dim[6]
+
+    # 3d bounding box corners
+    Box = np.array([[-l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2],
+                    [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2],
+                    [0, 0, 0, 0, h, h, h, h]])
+
+    R = rotz(yaw)
+    corners_3d = np.dot(R, Box) # corners_3d: (3, 8)
+
+    corners_3d[0,:] = corners_3d[0,:] + x
+    corners_3d[1,:] = corners_3d[1,:] + y
+    corners_3d[2,:] = corners_3d[2,:] + z
+
+    return np.transpose(corners_3d)
+
+
+def create_box_from_corners(corners, color=None):
+	'''
+	corners: 8 corners(x, y, z)
+	corners: array = 8*3
+	#         7 -------- 6
+	#        /|         /|
+	#       4 -------- 5 .
+	#       | |        | |
+	#       . 3 -------- 2
+	#       |/         |/
+	#       0 -------- 1
+	'''
+	# 12 lines in a box
+	lines = [[0, 1], [1, 2], [2, 3], [3, 0], 
+				[4, 5], [5, 6], [6, 7], [7, 4],
+				[0, 4], [1, 5], [2, 6], [3, 7]]
+	if color == None:
+		color = [1, 0, 0] # red
+	colors = [color for i in range(len(lines))]
+	line_set = o3d.geometry.LineSet()
+	line_set.points = o3d.utility.Vector3dVector(corners)
+	line_set.lines = o3d.utility.Vector2iVector(lines)
+	line_set.colors = o3d.utility.Vector3dVector(colors)
+
+	return line_set
+
+def rotation_matrix(roll, yaw, pitch):
+    R = np.array([[np.cos(yaw)*np.cos(pitch), 
+                   np.cos(yaw)*np.sin(pitch)*np.sin(roll)-np.sin(yaw)*np.cos(roll), 
+                   np.cos(yaw)*np.sin(pitch)*np.cos(roll)+np.sin(yaw)*np.sin(roll)],
+                  [np.sin(yaw)*np.cos(pitch), 
+                   np.sin(yaw)*np.sin(pitch)*np.sin(roll)+np.cos(yaw)*np.cos(roll), 
+                   np.sin(yaw)*np.sin(pitch)*np.cos(roll)-np.cos(yaw)*np.sin(roll)],
+                  [-np.sin(pitch), 
+                   np.cos(pitch)*np.sin(roll), 
+                   np.cos(pitch)*np.cos(roll)]])
+    return R
+
+def get_open3d_bbox(bbox):
+    # KITTI format to open3d
+    bbox_new = bbox.copy()
+    bbox_new[5] += bbox_new[2] / 2
+    # call open3d api
+    o3d_bbox = o3d.geometry.OrientedBoundingBox(center=bbox_new[3:6], R=rotation_matrix(0, bbox_new[6], 0), extent=bbox_new[0:3])
+    return o3d_bbox
+
+
+def create_box_from_dim(dim, color=None):
+    '''
+    dim: list(8) [h, w, l, x, y, z, yaw]
+    '''
+    box_corners = box_dim2corners(dim)
+    box = create_box_from_corners(box_corners, color)
+    return box
+
+
+def seperate_obj_points(points, boxes, box_types):
+    #--------------------------------------------------------------
+    # create boxes
+    #--------------------------------------------------------------
+    boxes_o3d = []
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points[:, :3])
+    all_object_indices = []
+    for i in range(boxes.shape[0]):
+        dim = boxes[i]
+        box_o3d = create_box_from_dim(dim)
+        box_o3d = get_open3d_bbox(dim)
+        boxes_o3d.append(box_o3d)
+
+        if box_types[i] in [3, 5]: # Pedestrian, Cyclist
+            # print(dim, box_types[i])
+            
+            indices = box_o3d.get_point_indices_within_bounding_box(pcd.points)
+            # print(indices)
+            all_object_indices += indices
+    
+    if len(all_object_indices) > 0:
+        obj_points = points[np.array(all_object_indices)]
+        non_obj_points = np.delete(points, np.array(all_object_indices), axis=0)
+    else:
+        obj_points = np.zeros([0, 4], dtype=np.float32)
+        non_obj_points = points
+
+    return obj_points, non_obj_points
